e2946396b4904f5ea7f2643482fec444afa652bf
Pierrot Damen <pierrotdamen84@gmail.com>
2020-10-11 12:41:19 +0200

    Web App done

--- auth.py
+++ auth.py
@@ -9,7 +9,7 @@ auth = Blueprint('auth', __name__)
 
 @auth.route("/login")
 def login():
-    return render_template('/login.html')
+    return render_template('login.html')
 
 
 @auth.route("/login", methods=["POST"])
