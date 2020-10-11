88f227334b7423056c4f27c0d00275efef1e0cde
Pierrot Damen <pierrotdamen84@gmail.com>
2020-10-11 09:44:12 +0200

    commit

--- app.py
+++ app.py
@@ -2,11 +2,19 @@ from flask import Flask, redirect, url_for, render_template, request, Response, 
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager, login_required, UserMixin, login_user
 from werkzeug.urls import url_parse
+from flask_sqlalchemy import SQLAlchemy
 
 # config
 app = Flask(__name__)
 Bootstrap(app)
-app.config.update(DEBUG=True, SECRET_KEY='2Png5xy8@')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    users = db.Column(db.String(200), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -15,13 +23,11 @@ def login():
 
 
 @app.route("/garage")
-@login_required
 def garage():
     return render_template("garage.html")
 
 
 @app.route("/logs")
-@login_required
 def logs():
     return render_template("logs.html")
 
